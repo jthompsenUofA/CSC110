7c68cfaab6eda3755cf7fca935b7e262d172d9b0
jthompsenUofA <143223388+jthompsenUofA@users.noreply.github.com>
2023-08-28 13:29:28 -0700

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,33 @@
+import math
+
+def triangle_area(a,b,c):
+    s = (a+b+c)/2
+    sa = s-a
+    sb = s-b
+    sc = s-c
+    sofeverything = s*sa*b*sc
+    area = sofeverything **.5
+    print(area)
+    return area
+
+def circle_area(radius):
+    squared = radius ** 2
+    area = 3.145 * squared
+    print(area)
+    return area
+
+def trapezoid_area(base_1, base_2, height):
+    base=base_1+base_2
+    area = .5*base*height
+    print(area)
+    return area
+
+
+def rectange_area(base, height):
+    area = base * height
+    print(area)
+    return area
+    
+rectange_area(5,4)
+triangle_area(5,5,5)
+circle_area(50)
